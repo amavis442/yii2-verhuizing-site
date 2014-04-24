20fe5cd99f98a2cf15572b10efb6fab5da5cb742
Patrick Teunissen <p.teunissen@ict-concept.nl>
2014-04-24 23:47:39 +0200

    Met Twitter bootstrap css framework

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,59 +1,91 @@
 <?php /* @var $this Controller */ ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="language" content="en" />
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="language" content="nl" />
 
-	<!-- blueprint CSS framework -->
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
-	<!--[if lt IE 8]>
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
-	<![endif]-->
+        <!-- blueprint CSS framework -->
 
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
+        <!-- Bootstrap core CSS -->
+        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/bootstrap-3.1.1/dist/css/bootstrap.min.css" />
 
-	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
-</head>
+        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->  
 
-<body>
+        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
+        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
 
-<div class="container" id="page">
+        
+        <?php Yii::app()->clientScript->registerCoreScript('jquery'); ?>
 
-	<div id="header">
-		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
-	</div><!-- header -->
+        <title><?php echo CHtml::encode($this->pageTitle); ?></title>
+    </head>
 
-	<div id="mainmenu">
-		<?php $this->widget('zii.widgets.CMenu',array(
-			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
-				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
-			),
-		)); ?>
-	</div><!-- mainmenu -->
-	<?php if(isset($this->breadcrumbs)):?>
-		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
-			'links'=>$this->breadcrumbs,
-		)); ?><!-- breadcrumbs -->
-	<?php endif?>
+    <body>
+        <?php Yii::app()->bootstrap->register(); ?>
+        <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Verhuizing</a>
+                </div>
+                <div class="navbar-collapse collapse">
+                    <?php
+                    $this->widget('zii.widgets.CMenu', array(
+                        'items' => array(
+                            array('label' => 'Home', 'url' => array('/site/index')),
+                            array('label' => 'About', 'url' => array('/site/page', 'view' => 'about')),
+                            array('label' => 'Contact', 'url' => array('/site/contact')),
+                            array('label' => 'Login', 'url' => array('/site/login'), 'visible' => Yii::app()->user->isGuest),
+                            array('label' => 'Logout (' . Yii::app()->user->name . ')', 'url' => array('/site/logout'), 'visible' => !Yii::app()->user->isGuest)
+                        ),
+                        'htmlOptions' => array('class' => 'nav navbar-nav navbar-center'),
+                    ));
+                    ?>
+                </div><!--/.nav-collapse -->
+            </div>
+        </div>
 
-	<?php echo $content; ?>
 
-	<div class="clear"></div>
+        <div class="container">
+            <div class="jumbotron">
+                <div id="header">
+                    <div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
+                </div><!-- header -->
 
-	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
-		All Rights Reserved.<br/>
-		<?php echo Yii::powered(); ?>
-	</div><!-- footer -->
+                <?php if (isset($this->breadcrumbs) && count($this->breadcrumbs) > 0): ?>
+                    <?php
+                        echo TbHtml::breadcrumbs($this->breadcrumbs);
+                    ?><!-- breadcrumbs -->
+                <?php endif ?>
 
-</div><!-- page -->
+                    
+                    
+                <?php echo $content; ?>
 
-</body>
+                <div class="clear"></div>
+
+                <div id="footer">
+                    Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
+                    All Rights Reserved.<br/>
+                    <?php echo Yii::powered(); ?>
+                </div><!-- footer -->
+            </div>
+        </div><!-- page -->
+
+
+        <script src="<?php echo Yii::app()->request->baseUrl; ?>/bootstrap-3.1.1/dist/js/bootstrap.min.js"></script>
+
+    </body>
 </html>
